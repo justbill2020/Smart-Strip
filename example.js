7f73060adbe26f6f3579eef581d87eedbcb09923
Bill Martin <bmartin@salientcdi.com>
2017-01-25 10:14:23 -0600

    'try' this

--- example.js
+++ example.js
@@ -31,18 +31,22 @@ board.on("ready", function() {
         var current_colors = [0,1,2,3,4];
         var current_pos = [0,1,2,3,4];
         var blinker = setInterval(function() {
+            try {
+                strip.color("#000"); // blanks it out
 
-            strip.color("#000"); // blanks it out
-
-            for (var i=0; i< current_pos.length; i++) {
-                if (++current_pos[i] >= strip.length) {
-                    current_pos[i] = 0;
-                    if (++current_colors[i] >= colors.length) current_colors[i] = 0;
+                for (var i=0; i< current_pos.length; i++) {
+                    if (++current_pos[i] >= strip.length) {
+                        current_pos[i] = 0;
+                        if (++current_colors[i] >= colors.length) current_colors[i] = 0;
+                    }
+                    strip.pixel(current_pos[i]).color(colors[current_colors[i]]);
                 }
-                strip.pixel(current_pos[i]).color(colors[current_colors[i]]);
-            }
 
-            strip.show();
+                strip.show();
+            } (e) {
+                console.log(e)
+            }
         }, 1000/fps);
+
     });
 });
