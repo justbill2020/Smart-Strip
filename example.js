8f2bdf2980638bace84f43ac6f5497fb5cf013a4
Bill Martin <bmartin@salientcdi.com>
2017-01-26 01:49:18 -0600

    different example

--- example.js
+++ example.js
@@ -24,26 +24,50 @@ board.on("ready", function() {
     });
 
     strip.on("ready", function() {
-
         console.log("Strip ready, let's go");
-
-        var colors = ["red", "green", "blue", "yellow", "cyan", "magenta", "white"];
-        var current_colors = [0,1,2,3,4];
-        var current_pos = [0,1,2,3,4];
-        var blinker = setInterval(function() {
-                strip.color("#000"); // blanks it out
-
-                for (var i=0; i< current_pos.length; i++) {
-                    if (++current_pos[i] >= strip.length) {
-                        current_pos[i] = 0;
-                        if (++current_colors[i] >= colors.length) current_colors[i] = 0;
-                    }
-                    strip.pixel(current_pos[i]).color(colors[current_colors[i]]);
-                    console.log('pos/color: '+current_pos,colors[current_colors[i]])
-                }
-
-                strip.show();
-        }, 1000/fps);
-
+        dynamicRainbow(fps);
     });
+
+    function dynamicRainbow( delay ){
+        console.log( 'dynamicRainbow' );
+
+        var showColor;
+        var cwi = 0; // colour wheel index (current position on colour wheel)
+        var foo = setInterval(function(){
+            if (++cwi > 255) {
+                cwi = 0;
+            }
+
+            for(var i = 0; i < strip.length; i++) {
+                showColor = colorWheel( ( cwi+i ) & 255 );
+                strip.pixel( i ).color( showColor );
+            }
+            strip.show();
+        }, 1000/delay);
+    }
+
+    // Input a value 0 to 255 to get a color value.
+    // The colors are a transition r - g - b - back to r.
+    function colorWheel( WheelPos ){
+        var r,g,b;
+        WheelPos = 255 - WheelPos;
+
+        if ( WheelPos < 85 ) {
+            r = 255 - WheelPos * 3;
+            g = 0;
+            b = WheelPos * 3;
+        } else if (WheelPos < 170) {
+            WheelPos -= 85;
+            r = 0;
+            g = WheelPos * 3;
+            b = 255 - WheelPos * 3;
+        } else {
+            WheelPos -= 170;
+            r = WheelPos * 3;
+            g = 255 - WheelPos * 3;
+            b = 0;
+        }
+        // returns a string with the rgb value to be used as the parameter
+        return "rgb(" + r +"," + g + "," + b + ")";
+    }
 });
