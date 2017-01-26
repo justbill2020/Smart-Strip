58f20af813500033d64126385ab517acba2f793c
Bill Martin <bmartin@salientcdi.com>
2017-01-26 10:32:55 -0600

    basic example... (#1)
    * add package.json
    
    * add example.js
    
    * fix node-pixel require statement
    
    * add raspi requirement
    
    * 'try' this
    
    * dropped the ball so now added 'catch'
    
    * don't try or catch
    
    * add interchange
    
    * loging
    
    * different example
    
    * 300 strips
    
    * blank then start
    
    * try catch again
    
    * try something
    
    * exit gracefully if you can
    
    * try hex instead of word add .vscode to gitignore

---
+++ example.js
@@ -0,0 +1,39 @@
+var five = require("johnny-five");
+var pixel = require("node-pixel");
+var raspi = require("raspi-io");
+
+var opts = {};
+opts.port = process.argv[2] || "";
+
+var board = new five.Board({
+    io: new raspi()
+});
+var strip = null;
+
+var fps = 20; // how many frames per second do you want to try?
+
+board.on("ready", function() {
+
+    console.log("Board ready, lets add light");
+
+    strip = new pixel.Strip({
+        color_order: pixel.COLOR_ORDER.GRB,
+        board: this,
+        controller: "I2CBACKPACK",
+        strips: [300],
+    });
+
+    strip.on("ready", function() {
+        board.on("exit", function(){
+            strip.color("black")
+            strip.show()
+        })
+        console.log("Strip ready, let's go");
+        strip.color("#000"); // blanks it out
+        console.log("#000")
+        strip.show()
+        strip.color("#fff")
+        strip.show()
+
+    });
+});
