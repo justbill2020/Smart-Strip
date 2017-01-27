7eb93402af7a4a4748baf73648440ca24d270e19
Bill Martin <bmartin@salientcdi.com>
2017-01-27 14:25:26 -0600

    Pr3 (#2)
    * update package.json
    
    * add config and blynk as well as main app
    
    This reverts commit 793704fd2ed4e09802e36e637699b1da84393c18.
    Revert "bugfix"
    
    This reverts commit d1b860e0a073295f0fcc9d2597090337b7c7cecc.
    Revert "bugfix"
    
    This reverts commit 99f381d5bddec7898accc8fef2082b94ff125ec5.
    bugfix
    
    bugfix
    
    add brightness
    
    debugging fix
    
    
    debugging fix
    
    debugging fix
    
    bugfix
    
    bugfix
    
    bug fix
    
    update package with debug script
    
    oops
    
    set ready vars
    
    update config.default.js
    
    update main.js
    
    add blynk to package
    
    added blynk, added config and main
    
    add config and blynk as well as main app

--- example.js
+++ example.js
@@ -2,6 +2,46 @@ var five = require("johnny-five");
 var pixel = require("node-pixel");
 var raspi = require("raspi-io");
 
+let config
+let firstRun = false
+try {
+  config = require("./config.json")
+} catch (e) {
+  let error = "Unknown Error"
+  config = require("./.config.default.json")
+  firstRun = true
+  if (typeof e.code !== 'undefined' && e.code === 'MODULE_NOT_FOUND') {
+    error = "'config.js' not found. \nYou can configure your mirror at the remote address below..."
+  } else if (typeof e.message !== 'undefined') {
+    console.log(e)
+    error = "Syntax Error. \nLooks like there's an error in your config file: " + e.message + '\n' +
+      'Protip: You might want to paste your config file into a JavaScript validator like http://jshint.com/'
+  }
+  console.log(error)
+}
+
+var ready = {}
+ready.board = false
+ready.strip = false
+ready.blynk = false
+ready.blynkOn = false
+var myColor = [0,0,0]
+
+function setStrip (state, color = "black") {
+    ready.blynkOn = state;
+    myColor = color;
+    if (ready.strip) {
+        strip.color(myColor);
+        if (ready.blynkOn) {
+            strip.show();
+        } else {
+            strip.off();
+        }
+    } else {
+        console.log("Cannot set strip. Strip Not Ready.");
+    }
+
+}
 var opts = {};
 opts.port = process.argv[2] || "";
 
@@ -10,7 +50,7 @@ var board = new five.Board({
 });
 var strip = null;
 
-var fps = 20; // how many frames per second do you want to try?
+var fps = config.strips.fps; // how many frames per second do you want to try?
 
 board.on("ready", function() {
 
@@ -20,20 +60,40 @@ board.on("ready", function() {
         color_order: pixel.COLOR_ORDER.GRB,
         board: this,
         controller: "I2CBACKPACK",
-        strips: [300],
+        strips: config.strips.leds,
     });
 
     strip.on("ready", function() {
         board.on("exit", function(){
-            strip.color("black")
-            strip.show()
+            strip.off();
         })
-        console.log("Strip ready, let's go");
-        strip.color("#000"); // blanks it out
-        console.log("#000")
-        strip.show()
-        strip.color("#fff")
-        strip.show()
-
+        console.log("Strip ready, let's go! Use Blynk to control lights");
     });
 });
+
+var Blynk = require('blynk-library');
+
+if (config.blynk.auth) {
+var blynk = new Blynk.Blynk(config.blynk.auth);
+
+var v0 = new blynk.VirtualPin(0);
+var v0 = new blynk.VirtualPin(1);
+
+v0.on('write', function(param) {
+  console.log('V0:', param);
+  setStrip(param === 0, myColor);
+});
+v1.on('write', function(param) {
+  console.log('V1:', param);
+  setStrip(ready.blynkOn, param);
+});
+
+
+blynk.on('connect', function() {
+    blynk.syncAll(); 
+    console.log("Blynk ready."); 
+});
+blynk.on('disconnect', function() { 
+    console.log("DISCONNECT"); 
+});
+}
